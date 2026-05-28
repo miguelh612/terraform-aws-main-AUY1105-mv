52da8594f0049f5ab04100f0499e5ac0c05e13ae
miguelh612 <mi.villagra@duocuc.cl>
2026-05-28 17:19:27 -0400

    Correcciones de formato

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ module "vpc" {
 }
 
 module "ec2" {
-  source = "github.com/miguelh612/terraform-aws-ec2-AUY1105-mv?ref=v1.0.2"
-  vpc_id = module.vpc.vpc_id
-  public_subnet_id = module.vpc.public_subnet_id
-  public_subnet_2_id = module.vpc.public_subnet_2_id
+  source                = "github.com/miguelh612/terraform-aws-ec2-AUY1105-mv?ref=v1.0.2"
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_id      = module.vpc.public_subnet_id
+  public_subnet_2_id    = module.vpc.public_subnet_2_id
   alb_security_group_id = module.alb.alb_security_group_id
 }
 
@@ -15,9 +15,9 @@ module "s3" {
 }
 
 module "alb" {
-  source = "github.com/miguelh612/terraform-aws-alb-AUY1105-mv?ref=v1.0.2"
-  vpc_id = module.vpc.vpc_id
-  public_subnet_id = module.vpc.public_subnet_id
+  source             = "github.com/miguelh612/terraform-aws-alb-AUY1105-mv?ref=v1.0.2"
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_id   = module.vpc.public_subnet_id
   public_subnet_2_id = module.vpc.public_subnet_2_id
-  ec2_instance_id = module.ec2.ec2_instance_id
+  ec2_instance_id    = module.ec2.ec2_instance_id
 }
