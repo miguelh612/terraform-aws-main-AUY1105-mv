0d6775c0dec800677d9f2d75299f0f40f389d71e
miguelh612 <mi.villagra@duocuc.cl>
2026-05-28 16:18:41 -0400

    Release estable

--- main.tf
+++ main.tf
@@ -1,11 +1,23 @@
 module "vpc" {
-  source = ""
+  source = "https://github.com/miguelh612/terraform-aws-vpc-AUY1105-mv?ref=1.0.0"
 }
 
 module "ec2" {
-  source = ""
+  source = "https://github.com/miguelh612/terraform-aws-ec2-AUY1105-mv?ref=1.0.0"
+  vpc_id = module.vpc.vpc_id
+  public_subnet_id = module.vpc.public_subnet_id
+  public_subnet_2_id = module.vpc.public_subnet_2_id
+  alb_security_group_id = module.alb.alb_security_group_id
 }
 
 module "s3" {
-  source = ""
+  source = "https://github.com/miguelh612/terraform-aws-s3-AUY1105-mv?ref=1.0.0"
+}
+
+module "alb" {
+  source = "https://github.com/miguelh612/terraform-aws-alb-AUY1105-mv?ref=1.0.0"
+  vpc_id = module.vpc.vpc_id
+  public_subnet_id = module.vpc.public_subnet_id
+  public_subnet_2_id = module.vpc.public_subnet_2_id
+  ec2_instance_id = module.ec2.ec2_instance_id
 }
